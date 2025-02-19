bae98e04281b4d4ca9cf7be580f7621d3d715f74
Rama Diaz <ramadiaz221@gmail.com>
2025-02-20 00:35:17 +0700

    INIT of /internal/blog/featured

--- internal/routers/blog_router.go
+++ internal/routers/blog_router.go
@@ -14,6 +14,7 @@ func BlogRoutes(r *gin.RouterGroup, compControllers controllers.CompControllers)
 		blogGroup.GET("/all", compControllers.FindAll)
 		blogGroup.GET("/slug", compControllers.FindBySlug)
 		blogGroup.GET("/uuid", compControllers.FindByUUID)
+		blogGroup.GET("/featured", compControllers.FindFeaturedBlogs)
 
 		featuredGroup := blogGroup.Group("/featured")
 		{
